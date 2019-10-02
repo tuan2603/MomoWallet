7d13b1d7beba89f39a623674490711f34c106064
tuan2603 <tuan260387@gmail.com>
2019-10-02 08:49:18 +0700

    first commit

---
+++ src/ios/MomoWallet.m
@@ -0,0 +1,28 @@
+/********* MomoWallet.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface MomoWallet : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation MomoWallet
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
